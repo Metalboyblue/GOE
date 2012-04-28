2bbd575994fe8cf5d9e02768c7a21bc0601e0271
Travis Baker <travis_baker@bellsouth.net>
2012-04-28 10:20:20 -0400

    Initial Commit

---
+++ GladiatorsOfEden/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GladiatorsOfEden
+//
+//  Created by me on 4/28/12.
+//  Copyright MetalBoyBlue 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
